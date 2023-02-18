30ecd2b6b21534ca00b3e7e2c7539a9212a66488
PaulaaaArdelean <115561126+PaulaaaArdelean@users.noreply.github.com>
2023-02-18 13:13:14 +0200

    .

--- Final/Models/Rochie.cs
+++ Final/Models/Rochie.cs
@@ -10,12 +10,12 @@ namespace Final.Models
         public int ID { get; set; }
 
         [Display(Name = "Denumirea rochiei")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$", ErrorMessage = "Denumirea rochiei trebuie sa fie inceapa cu majuscula")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$", ErrorMessage = "Denumirea rochiei trebuie sa inceapa cu majuscula")]
 
         public string Denumire { get; set; }
 
         [Column(TypeName = "decimal(10,2)")]
-        [Range(0.01, 100000)]
+        [Range(0.01, 100000, ErrorMessage ="Pretul nu poate fi mai mic de 0.01 RON sau mai mare de 100000 RON")]
         public decimal Pret { get; set; }
         
         public int? DesignerID { get; set; }
